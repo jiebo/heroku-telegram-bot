beed5737b01e60df749975e0403cfdef3a76cf92
Ti Jie Bo <tijiebo@hotmail.com>
2017-10-06 15:48:02 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,10 +11,25 @@ token = os.environ['TELEGRAM_TOKEN']
 some_api_token = os.environ['SOME_API_TOKEN']
 #             ...
 
-# If you use redis, install this add-on https://elements.heroku.com/addons/heroku-redis
-r = redis.from_url(os.environ.get("REDIS_URL"))
-
 #       Your bot code below
 # bot = telebot.TeleBot(token)
 # some_api = some_api_lib.connect(some_api_token)
 #              ...
+def handle(msg):
+    chat_id = msg['chat']['id']
+    command = msg['text']
+
+    print 'Got command: %s' % command
+
+    if command == '/roll':
+        bot.sendMessage(chat_id, random.randint(1,6))
+    elif command == '/time':
+        bot.sendMessage(chat_id, str(datetime.datetime.now()))
+
+bot = telepot.Bot('434900295:AAGxz2ujTfycUKYPJUspj87wtWaAToiSTkU')
+
+MessageLoop(bot, handle).run_as_thread()
+print 'I am listening ...'
+
+while 1:
+  time.sleep(10)
