2658ea2e6cafee4c57d05d32b243f3a87d557c18
Jie Bo Ti <tijiebo@hotmail.com>
2017-11-17 11:06:20 +0800

    something

--- bot.py
+++ bot.py
@@ -1,18 +1,19 @@
 # -*- coding: utf-8 -*-
+"""Telegram bot"""
 import os
+import time
 import telebot
 import cloudinary
 import cloudinary.uploader
 import cloudinary.utils
 import cloudinary.api
-import time
 
 # import some_api_lib
 # import ...
 
 # Example of your code beginning
 #           Config vars
-token = os.environ['TELEGRAM_TOKEN']
+TOKEN = os.environ['TELEGRAM_TOKEN']
 # some_api_token = os.environ['SOME_API_TOKEN']
 #             ...
 
@@ -21,9 +22,7 @@ token = os.environ['TELEGRAM_TOKEN']
 # some_api = some_api_lib.connect(some_api_token)
 #              ...
 
-BOT = telebot.TeleBot(token)
-updates = BOT.get_updates()
-startDate
+BOT = telebot.TeleBot(TOKEN)
 cloudinary.config(
     cloud_name="eu-sep",
     api_key="511481921314569",
@@ -32,53 +31,37 @@ cloudinary.config(
 
 
 def upload(photo):
+    """Uploads user-uploaded image onto Cloudinary"""
     cloudinary.uploader.upload(
-        "https://api.telegram.org/file/bot" + token + "/" +
+        "https://api.telegram.org/file/bot" + TOKEN + "/" +
         BOT.get_file(photo.photo[-1].file_id).file_path)
 
 
-photo_array = ['https://avatars3.githubusercontent.com/u/10268386?s=400&u=c22979fe17a17df6aa32d3cf7326e8370160dd47&v=4',
-               'https://avatars1.githubusercontent.com/u/4401928?s=400&v=4']
-
-photo_array = ['https://avatars3.githubusercontent.com/u/10268386?s=400&u=c22979fe17a17df6aa32d3cf7326e8370160dd47&v=4',
-               'https://avatars1.githubusercontent.com/u/4401928?s=400&v=4']
-
-    print(message)
-    upload(message)
-
-
-def upload_picture(message):
-    print(message)
-    upload(message)
-    BOT.reply_to(
-        message, "Please use the attach image button instead of attaching a document")
+PHOTO_ARRAY = [
+    'https://avatars3.githubusercontent.com/u/10268386?u=c22979fe17a17df6aa32d3cf7326e8370160dd47',
+    'https://avatars1.githubusercontent.com/u/4401928?s=400&v=4'
+]
 
 
 @BOT.message_handler(content_types=['document'])
 def user_uploads_document(message):
-    upload(photo)
-
+    """When user uses the wrong upload button"""
+    BOT.reply_to(
         message, "Please use the attach image button instead of attaching a document")
 
 
-    BOT.reply_to(message, "Testing 123")
-
-def user_uploads_photo(photo):
-    upload(photo)
-
-    for url in photo_array:
-        BOT.send_message(
-            message.chat.id, "[Options](" + url + ")", parse_mode = "Markdown")
-
+@BOT.message_handler(commands=['start', 'help'])
 def send_welcome(message):
+    """Default command"""
     BOT.reply_to(message, "Testing 123")
 
 
-@BOT.message_handler(func = lambda message: True)
+@BOT.message_handler(func=lambda message: True)
 def echo_all(message):
-    for url in photo_array:
+    """Method returns images in PHOTO_ARRAY as Options"""
+    for url in PHOTO_ARRAY:
         BOT.send_message(
-            message.chat.id, "[Options](" + url + ")", parse_mode = "Markdown")
+            message.chat.id, "[Options](" + url + ")", parse_mode="Markdown")
 
 
 BOT.polling()
