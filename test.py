355e0fa577f80484b8a70f1398a1ce96d2335290
Jie Bo Ti <tijiebo@hotmail.com>
2018-01-19 16:02:23 +0800

    New tests

--- test.py
+++ test.py
@@ -4,13 +4,21 @@ import util
 
 class MyTestCase(unittest.TestCase):
 
-    def test_voting_mechanism(self):
+    def test_voting_mechanism_failure(self):
+        test_array = [0]
+        message = '/option1'
+        util.vote_option(message, test_array)
+        self.assertEqual(test_array[0], 0)
+
+    def test_voting_mechanism_success(self):
         test_array = [0]
         message = '/option1'
         util.vote_option(message, test_array)
         self.assertEqual(test_array[0], 1)
 
-
-
 if __name__ == '__main__':
-    unittest.main()
+   log_file = 'log_file.txt'
+   f = open(log_file, "w")
+   runner = unittest.TextTestRunner(f)
+   unittest.main(testRunner=runner)
+   f.close()
